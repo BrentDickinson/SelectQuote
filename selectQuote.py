f87ae364f4a9bea5426c3cc74e817c4db236855e
Brent Dickinson <Brent.Dickinson@gmail.com>
2012-03-16 18:44:45 -0700

    Change code formatting

--- selectQuote.py
+++ selectQuote.py
@@ -1,23 +1,25 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 import re
+
 
 class selectquoteCommand(sublime_plugin.TextCommand):
     counter = 0
 
-    def createStop(self,match):
+    def createStop(self, match):
         self.counter = self.counter + 1
-        return(match.group(1) + '${'+str(self.counter)+':' + match.group(2) + '}' + match.group(1))
+        return(match.group(1) + '${' + str(self.counter) + ':' + match.group(2) + '}' + match.group(1))
 
     def run(self, edit):
         for region in self.view.sel():
                 line = self.view.line(region)
                 line_contents = self.view.substr(line)
-                lregion = sublime.Region(line.a,line.b)
+                lregion = sublime.Region(line.a, line.b)
 
-                contents = line_contents.replace("$","\$");
-                contents=re.sub("([\"'])(.*?)\\1",self.createStop,contents,re.M)
+                contents = line_contents.replace("$", "\$")
+                contents = re.sub("([\"'])(.*?)\\1", self.createStop, contents, re.M)
                 self.view.sel().clear()
                 self.view.sel().add(lregion)
 
                 self.view.run_command('insert_snippet', {'contents': contents})
-                self.counter=0;
+                self.counter = 0
